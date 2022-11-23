480067fd8b32b4c648188cef664c53aa75bf072c
Holla die Waldfee <x7usozafi@relay.firefox.com>
2022-11-23 14:54:28 +0100

    added text area and todo

--- main.java
+++ main.java
@@ -1,11 +1,17 @@
-import javax.swing.JFrame;
-
+import javax.swing.*;
+import java.awt.Font;
 public class FrameBeispiel
 {
     public static void main(String[] args)
     {
-        JFrame meinFrame = new JFrame("Beispiel JFrame");
-        meinFrame.setSize(200,200);
-        meinFrame.setVisible(true);
+        JFrame root = new JFrame("Beispiel JFrame");
+        root.setSize(200,200);
+
+
+        JTextArea text = new JTextArea();
+        text.setFont(new Font("Hack", Font.PLAIN, 20));
+        root.add(text);
+
+        root.setVisible(true);
     }
 }
