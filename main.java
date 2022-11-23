305460f721078759f9d1c656864078dc9e1e90b3
Holla die Waldfee <x7usozafi@relay.firefox.com>
2022-11-23 13:40:40 +0100

    empty frame

---
+++ main.java
@@ -0,0 +1,11 @@
+import javax.swing.JFrame;
+
+public class FrameBeispiel
+{
+    public static void main(String[] args)
+    {
+        JFrame meinFrame = new JFrame("Beispiel JFrame");
+        meinFrame.setSize(200,200);
+        meinFrame.setVisible(true);
+    }
+}
