0a3f1eb6ab135d1821db7322ab51ea15a2ef7952
Holla die Waldfee <x7usozafi@relay.firefox.com>
2022-11-24 19:31:37 +0100

    added linenumbers

--- main.java
+++ main.java
@@ -1,5 +1,10 @@
 import javax.swing.*;
-import java.awt.Font;
+import javax.swing.event.*;
+import java.awt.*;
+
+
+
+
 public class FrameBeispiel
 {
     public static void main(String[] args)
@@ -7,13 +12,59 @@ public class FrameBeispiel
         JFrame root = new JFrame("Beispiel JFrame");
         root.setSize(800, 600);
 
-        JTextArea text = new JTextArea();
+        JTextArea text = new JTextArea("a");
         text.setFont(new Font("Hack", Font.PLAIN, 13));
+
+        JTextArea line_numbers = new JTextArea("1\n2\n3");
+        line_numbers.setFont(new Font("Hack", Font.PLAIN, 13));
+        line_numbers.setBackground(Color.LIGHT_GRAY);
+        line_numbers.setEditable(false);
+
+        text.getDocument().addDocumentListener(new Line_Number_Inserter(text, line_numbers));
 
         JScrollPane scroll_plane = new JScrollPane(text);
 
-        root.getContentPane().add(scroll_plane);
+        scroll_plane.getViewport().add(text);
+        scroll_plane.setRowHeaderView(line_numbers);
+
+        root.add(scroll_plane);
 
         root.setVisible(true);
+    }
+}
+
+
+
+public class Line_Number_Inserter implements DocumentListener {
+    private JTextArea text, line_numbers;
+
+    Line_Number_Inserter(JTextArea ptext, JTextArea pline_numbers) {
+        this.text = ptext;
+        this.line_numbers = pline_numbers;
+    }
+
+    private void update_numbers() {
+        String result = "1";
+        int line_count = 1;
+        String input = this.text.getText();
+        int length = input.length();
+        //System.out.println(this.text.getText());
+        for (int i = 0; i < length; i++) {
+            if (input.charAt(i) == '\n') {
+                line_count++;
+                result = result + "\n" + Integer.toString(line_count);
+            }
+        }
+        this.line_numbers.setText(result);
+    }
+
+    public void insertUpdate(DocumentEvent e) {
+        update_numbers();
+    }
+    public void removeUpdate(DocumentEvent e) {
+        update_numbers();
+    }
+    public void changedUpdate(DocumentEvent e) {
+        //Plain text components do not fire these events
     }
 }
