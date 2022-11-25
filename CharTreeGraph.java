62f68a95901967ed1152143b486f23ce652fac26
Tobias Held <tobias.hd4@gmx.de>
2022-11-25 11:56:52 +0100

    updated

---
+++ CharTreeGraph.java
@@ -0,0 +1,25 @@
+class CharTreeGraph{
+    class CharTreeNode{
+        public CharTreeNode[] children;     
+        public char c;
+        public boolean isEnd;
+        void addStringToGraph(String str){
+            for(int i=0; i<children.length;i++){
+                if(children[i].c==str.charAt(0)){
+                    if(str.length()==1){
+                        
+                    }
+                }
+            }
+        }    
+    }
+    CharTreeNode[] nodes;
+    void addStringToGraph(String str){
+        
+    }
+
+
+
+
+
+}
