95f1350becc025daed6f834437a1ea920d437883
Holla die Waldfee <x7usozafi@relay.firefox.com>
2022-11-26 10:01:43 +0100

    changed exit behaviour. copied everything back into a single file due to severe issues with java

--- main.java
+++ main.java
@@ -3,6 +3,7 @@ import javax.swing.event.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.*;
 
 
 
@@ -15,6 +16,7 @@ public class Tabi_Window implements ActionListener
     JMenuItem save_as;
     public Tabi_Window() {
         JFrame root = new JFrame("Beispiel JFrame");
+        root.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         root.setSize(800, 600);
 
         JMenuBar menubar = new JMenuBar();
@@ -113,4 +115,59 @@ public class Line_Number_Inserter implements DocumentListener {
     public void changedUpdate(DocumentEvent e) {
         //Plain text components do not fire these events
     }
+}
+
+
+
+public class Filesystem {
+    // TODO add Error popup
+    public String read(String file_name) {
+        // This will reference one line at a time
+        String line = null;
+
+        try {
+            String result = "";
+            // FileReader uses default encoding.
+            BufferedReader bufferedReader = new BufferedReader(new FileReader(file_name));
+
+            while((line = bufferedReader.readLine()) != null) {
+                result += line + "\n";
+            }
+
+            bufferedReader.close();
+            return result;
+        }
+        catch(FileNotFoundException ex) {
+            System.out.println("Unable to open file '" + file_name + "'");
+        }
+        catch(IOException ex) {
+            System.out.println("Error reading file '" + file_name + "'");
+            // Or we could just do this:
+            // ex.printStackTrace();
+        }
+        return null;
+    }
+
+    public boolean write(String file_name, String file_content, boolean allow_overwrite) {
+        try {
+            //Create file
+            if (new File(file_name).createNewFile() || allow_overwrite) {
+                //Write to file
+                FileWriter myWriter = new FileWriter(file_name);
+                myWriter.write(file_content);
+                myWriter.close();
+                return true;
+            }
+        }
+        catch (IOException e) {
+            System.out.println("An error occurred while creating/writing a file");
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    //public static void main(String [] args) {
+    //    new Filesystem().write("/home/waldfee/test.txt", "Hello Filesystem", false);
+    //    System.out.println(new Filesystem().read("/home/waldfee/test.txt"));
+    //}
 }
