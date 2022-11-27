274f5f229a062b8e3e7305e719cec32bbfc57fed
Holla die Waldfee <x7usozafi@relay.firefox.com>
2022-11-27 19:35:44 +0100

    added tabs. added functionality for new/open file

--- main.java
+++ main.java
@@ -14,10 +14,12 @@ public class Tabi_Window implements ActionListener
     JMenuItem open_recent;
     JMenuItem save_file;
     JMenuItem save_as;
-    public Tabi_Window() {
-        JFrame root = new JFrame("Beispiel JFrame");
-        root.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        root.setSize(800, 600);
+    JFrame root;
+    JTabbedPane tab_manager;
+    public Tabi_Window(String[] args) {
+        this.root = new JFrame("Tabi");
+        this.root.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        this.root.setSize(800, 600);
 
         JMenuBar menubar = new JMenuBar();
         JMenu menu = new JMenu("File");
@@ -41,42 +43,101 @@ public class Tabi_Window implements ActionListener
         menu.add(save_as);
 
         menubar.add(menu);
-        root.setJMenuBar(menubar);
+        this.root.setJMenuBar(menubar);
 
-        JTextArea text = new JTextArea("a");
-        text.setFont(new Font("Hack", Font.PLAIN, 13));
+        this.tab_manager = new JTabbedPane();
+
+        if (args.length == 0) {
+            new Text_Tab(this.tab_manager, "");
+        }
+        else {
+            for (String file_name : args) {
+                new Text_Tab(this.tab_manager, file_name);
+            }
+        }
+
+        this.root.add(this.tab_manager);
+
+        this.root.setVisible(true);
+    }
+
+    public void actionPerformed (ActionEvent event){
+        if (event.getSource() == this.new_file) {
+            new Text_Tab(this.tab_manager, "");
+        }
+        else if (event.getSource() == this.open_file) {
+
+            JFileChooser fileChooser = new JFileChooser();
+            if (fileChooser.showOpenDialog(this.root) == JFileChooser.APPROVE_OPTION) {
+                new Text_Tab(this.tab_manager, fileChooser.getSelectedFile().getAbsolutePath());
+            }
+            else {
+               return;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        new Tabi_Window(args);
+    }
+}
+
+
+
+public class Text_Tab {
+    boolean unsaved_changes = false;
+    String file_name = "";
+    JTextArea text_area;
+    Text_Tab(JTabbedPane tab_manager, String pfile_name) {
+        System.out.println(pfile_name);
+        this.text_area = new JTextArea();
+        this.text_area.setFont(new Font("Hack", Font.PLAIN, 13));
 
         JTextArea line_numbers = new JTextArea();
         line_numbers.setFont(new Font("Hack", Font.PLAIN, 13));
         line_numbers.setBackground(Color.LIGHT_GRAY);
         line_numbers.setEditable(false);
 
-        text.getDocument().addDocumentListener(new Line_Number_Inserter(text, line_numbers));
+        this.text_area.getDocument().addDocumentListener(new Line_Number_Inserter(this.text_area, line_numbers));
 
-        JScrollPane scroll_plane = new JScrollPane(text);
+        JScrollPane scroll_plane = new JScrollPane(this.text_area);
 
-        scroll_plane.getViewport().add(text);
+        scroll_plane.getViewport().add(this.text_area);
         scroll_plane.setRowHeaderView(line_numbers);
 
-        root.add(scroll_plane);
+        tab_manager.add("New", scroll_plane);
 
-        root.setVisible(true);
-    }
-
-    public void actionPerformed (ActionEvent event){
-        System.out.println("the menu got clicked");
-        if (event.getSource() == this.new_file) {
-            System.out.println("new file");
+        if (pfile_name != "") {
+            this.file_name = pfile_name;
+            // load file
+            this.text_area.setText(new Filesystem().read(this.file_name));
         }
-        /*switch (event.getSource()) {
-            case this.new_file:
-                System.out.println("here");
-                break;
-        }*/
+
+        // Make this the selected tab
+        tab_manager.setSelectedIndex(tab_manager.getTabCount()-1);
     }
 
-    public static void main(String[] args) {
-        new Tabi_Window();
+    public void save_document() {
+        if (this.file_name == "") {
+            //ask user where to save file
+            // TODO
+
+            JFrame parentFrame = new JFrame();
+
+            JFileChooser fileChooser = new JFileChooser();
+            fileChooser.setDialogTitle("Specify a file to save");
+
+            int userSelection = fileChooser.showSaveDialog(parentFrame);
+
+            if (userSelection == JFileChooser.APPROVE_OPTION) {
+                File fileToSave = fileChooser.getSelectedFile();
+                System.out.println("Save as file: " + fileToSave.getAbsolutePath());
+            }
+        }
+        //if (this.file_name == "") {
+        //    return;
+        //}
+        new Filesystem().write(this.file_name, this.text_area.getText());
     }
 }
 
@@ -148,22 +209,25 @@ public class Filesystem {
         return null;
     }
 
-    public boolean write(String file_name, String file_content, boolean allow_overwrite) {
+    public boolean write(String file_name, String file_content) {
         try {
             //Create file
-            if (new File(file_name).createNewFile() || allow_overwrite) {
-                //Write to file
-                FileWriter myWriter = new FileWriter(file_name);
-                myWriter.write(file_content);
-                myWriter.close();
-                return true;
-            }
+            new File(file_name).createNewFile();
+            //Write to file
+            FileWriter myWriter = new FileWriter(file_name);
+            myWriter.write(file_content);
+            myWriter.close();
+            return true;
         }
         catch (IOException e) {
             System.out.println("An error occurred while creating/writing a file");
             e.printStackTrace();
         }
         return false;
+    }
+
+    public boolean does_file_exist(String file_name) {
+        return new File(file_name).isFile();
     }
 
     //public static void main(String [] args) {
