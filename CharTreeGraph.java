9bd86cf2817b5aa71d1ee407f598d9e878df1699
Tobias Held <tobias.hd4@gmx.de>
2022-11-28 08:42:22 +0100

    Added CharTreeGraph

--- CharTreeGraph.java
+++ CharTreeGraph.java
@@ -3,23 +3,91 @@ class CharTreeGraph{
         public CharTreeNode[] children;     
         public char c;
         public boolean isEnd;
-        void addStringToGraph(String str){
-            for(int i=0; i<children.length;i++){
+        public String key;
+        public int currentChild;
+        public CharTreeNode(char c,boolean isEnd,String key){
+            this.c=c;
+            this.isEnd=isEnd;
+            children= new CharTreeNode[16];
+            currentChild=0;
+            if(isEnd){
+                this.key=key;
+            }
+        }
+        void addStringToGraph(String str,String key){
+            boolean hasFound= false;
+            
+            for(int i=0; i<currentChild;i++){
                 if(children[i].c==str.charAt(0)){
-                    if(str.length()==1){
+                    hasFound=true;
+                    str=str.substring(1);
+                    children[i].addStringToGraph(str, key);
+                }
+            }
+            if(!hasFound){
+                int insertIndex=currentChild;
+                if(currentChild==children.length){
 
+                
+                    
+                    CharTreeNode[] newChildren= new CharTreeNode[children.length*2];
+                    for(int i=0; i<children.length;i++){
+                        newChildren[i]=children[i];
+                    }
+                    children=newChildren;
+                    
+                }
+                
+                children[insertIndex]= new CharTreeNode(str.charAt(0),str.length()==1,key);
+                if(str.length()!=1){
+                    str=str.substring(1);
+                    children[insertIndex].addStringToGraph(str, key);
+                }
+                insertIndex++;
+            }
+           
+        } 
+        public String searchForToken(String token){
+            for(int i=0; i<this.currentChild;i++){
+                
+                if(children[i].c==token.charAt(0)){
+                    //token is at the end
+                    if(token.length()==1){
+                        if(children[i].isEnd){
+                            return children[i].key;
+                        }else{
+                            return null;
+                        }
+                    }else{
+                        token=token.substring(1);
+                        String res= children[i].searchForToken(token);
+                        if(res==null){
+                            if(this.isEnd){
+                                return this.key;
+                            }else{
+                                return null;
+                            }
+                        }
                     }
                 }
             }
-        }    
+            //if no child was found
+            if(this.isEnd){
+                return this.key;
+            }else{
+                return null;
+            }
+        }  
     }
-    private CharTreeNode[] nodes;
+    private CharTreeNode node;
+    public CharTreeGraph(){
+        node= new CharTreeNode('a', false, null);
+
+    }
     void addStringToGraph(String str){
-        
+        node.addStringToGraph(str, str);
     }
-
-
-
-
-
+    String searchForToken(String token){
+        	return node.searchForToken(token);
+    }
 }
