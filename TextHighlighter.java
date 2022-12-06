52102d9e76a4909222181d166ac4eeb7ba8c2206
Tobias Held <tobias.hd4@gmx.de>
2022-12-06 14:32:18 +0100

    uwu

--- TextHighlighter.java
+++ TextHighlighter.java
@@ -1,5 +1,6 @@
 import javax.swing.JTextArea;
 import javax.swing.JTextPane;
+import javax.swing.SwingUtilities;
 import javax.swing.event.CaretEvent;
 import javax.swing.event.CaretListener;
 import javax.swing.plaf.synth.ColorType;
@@ -16,16 +17,19 @@ public class TextHighlighter implements CaretListener{
     public TextHighlighter(JTextPane area){
         tokenGraph=new CharTreeGraph();
         tokens=new Token[2];
-        tokens[0]= new Token("null",0xFFFF0000);
-        tokens[1]=new Token("uwu",0xFF000000);
+        tokens[0]= new Token("null",0x00FF0000);
+        tokens[1]=new Token("uwu",0x000000FF);
 
         tokenGraph.addTokenToGraph(tokens[0]);
         tokenGraph.addTokenToGraph(tokens[1]);
         this.area=area;
     }
+    CharTreeGraph getGraph(){
+        return this.tokenGraph;
+    }
     @Override
     public void caretUpdate(CaretEvent e) {
-         
+         /* 
         String thisLine;
         //get current line
         String text= area.getText();
@@ -36,9 +40,7 @@ public class TextHighlighter implements CaretListener{
             if(text.charAt(i)=='\n'){
                 lastIndex=i;
                 break;
-            }
-
-            
+            } 
         }
         
         for(int i=dotPos-1;i>=0;i--){
@@ -47,29 +49,33 @@ public class TextHighlighter implements CaretListener{
                 firstIndex=i;
                 break;
             }
-            
-        }
-        
+        } 
         if(firstIndex>=lastIndex)return;
-        
         StyledDocument sDoc=area.getStyledDocument();
         thisLine= text.substring(firstIndex, lastIndex);
         for(int i=0; i<thisLine.length();i++){
             Token t= tokenGraph.searchForToken(thisLine.substring(i, thisLine.length()-1));
             if(t!=null){
-                //TODO: change location to outside doc listener
                 SimpleAttributeSet colorAttributeSet=new SimpleAttributeSet();
                 StyleConstants.setForeground(colorAttributeSet,new Color(t.color));
                 StyleConstants.setBackground(colorAttributeSet, Color.WHITE);
                 StyleConstants.setUnderline(colorAttributeSet, Boolean.FALSE );
-                StyleConstants.setBold(colorAttributeSet, true);
-                sDoc.setCharacterAttributes(firstIndex+i,t.tokenName.length(),colorAttributeSet,true);
+                StyleConstants.setBold(colorAttributeSet, false);
+                int startIndex= firstIndex+i;
+                SwingUtilities.invokeLater(new Runnable(){
+                    @Override
+                    public void run() {
+                        sDoc.setCharacterAttributes(startIndex,t.tokenName.length(),colorAttributeSet,true);
+                    }});
+                //TODO: change location to outside doc listener
+                
+                //sDoc.setCharacterAttributes(firstIndex+i,t.tokenName.length(),colorAttributeSet,true);
                 i+=t.tokenName.length()-1;
             }
 
         }
 
-        
+        */
         
     }
     
